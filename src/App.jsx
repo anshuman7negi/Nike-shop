49561f93b6890392eae16679b51d3647351cc2ad
anshuman7negi <anshumannegi777@gmail.com>
2023-11-24 13:27:59 +0530

    setup the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+      <p className="text-3xl">
+        Click on the Vite and React logos to learn more
+      </p>
+  )
+}
+
+export default App
