336e37282ad67c198be07abb19794b935e8dae02
anshuman7negi <anshumannegi777@gmail.com>
2023-11-25 13:43:27 +0530

    checking deploy

--- src/pages/main/Footer.jsx
+++ src/pages/main/Footer.jsx
@@ -3,6 +3,7 @@ const Footer = () => {
     return(
         <footer className="p-2" style={{ boxShadow: '0 0 5px rgba(255, 255, 255, 0.8)' }}>
             <p className="text-center text-lg font-bold">Made with <span className="text-red-500 text-2xl">&hearts;</span> by Anshuman Negi</p>
+            
         </footer>
     )
 }
